88769d89e4293724998cf0734b447d4975ceb2b0
Sabina Dědečková <sdmsi666@gmail.com>
2021-05-01 14:15:27 +0200

    úkol č.4 - done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-// úkol 2. - hide hamburger
+// úkol č.2 - hide hamburger
 
 const navEl = document.querySelector('nav');
 const navAEl = document.querySelectorAll('nav a');
@@ -14,4 +14,23 @@ navBtnEl.addEventListener('click', hideNav);
 
 navAEl.forEach((elm) => {
   elm.addEventListener('click', hideNav);
+});
+
+//úkol č. 4
+
+let ordered = false;
+
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', () => {
+  if (ordered === false) {
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    ordered = true;
+    orderBtn.textContent = 'Zrušit';
+  } else {
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    orderBtn.textContent = 'Objednat';
+    ordered = false;
+  }
 });
