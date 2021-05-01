ddd25e3f381e717257151778f990a810875cb11c
Sabina Dědečková <sdmsi666@gmail.com>
2021-05-01 12:00:24 +0200

    úkol č.3 - done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log('funguju!');
+// úkol 2. - hide hamburger
+
+const navEl = document.querySelector('nav');
+const navAEl = document.querySelectorAll('nav a');
+
+const hideNav = () => {
+  navEl.classList.toggle('nav-closed');
+};
+
+const navBtnEl = document.querySelector('#nav-btn');
+navBtnEl.addEventListener('click', hideNav);
+
+navAEl.forEach((elm) => {
+  elm.addEventListener('click', hideNav);
+});
